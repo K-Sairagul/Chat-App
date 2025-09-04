706c3ade09b4f0db69b97861361825979326bd83
sairagul-k <sairagul.cs21@bitsathy.ac.in>
2025-09-04 09:04:59 +0530

    new

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,12 +21,7 @@ const __dirname = path.resolve();
 
 app.use(express.json());
 app.use(cookieParser());
-app.use(
-  cors({
-    origin: "http://localhost:5173" ,
-    credentials: true,
-  })
-);
+app.use(cors({ origin: true, credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
