bff07e5d48984878023e6facdf0a7a4c0fdf5df7
sairagul-k <sairagul.cs21@bitsathy.ac.in>
2025-09-04 08:43:28 +0530

    new update

--- backend/src/index.js
+++ backend/src/index.js
@@ -20,13 +20,20 @@ const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
 app.use(express.json());
+
 app.use(cookieParser());
+const allowedOrigins = [
+  "http://localhost:5173",
+  "https://chat-app-axyh.onrender.com"
+];
+
 app.use(
   cors({
-    origin: "http://localhost:5173" ,
+    origin: allowedOrigins,
     credentials: true,
   })
 );
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
